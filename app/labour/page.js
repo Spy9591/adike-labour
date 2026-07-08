7049304844b4541ecc3dec923ea4ebd6f863c8f4
Spy9591 <93334073+Spy9591@users.noreply.github.com>
2026-07-08 21:43:13 +0530

    Update page.js

--- app/labour/page.js
+++ app/labour/page.js
@@ -1,8 +1,3 @@
 export default function LabourPage() {
-  return (
-    <div style={{ padding: "40px" }}>
-      <h1>👷 ಕಾರ್ಮಿಕ ನೋಂದಣಿ</h1>
-      <p>ಕಾರ್ಮಿಕ ವಿವರಗಳನ್ನು ಇಲ್ಲಿ ಸೇರಿಸಲಾಗುತ್ತದೆ.</p>
-    </div>
-  );
+  return <h1>Labour Page</h1>;
 }
