49bcaf8cdef492f6638aed8b394c454210d78987
Spy9591 <93334073+Spy9591@users.noreply.github.com>
2026-07-08 21:05:09 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,20 +1,11 @@
 export const metadata = {
   title: "ಅಡಿಕೆ ಕಾರ್ಮಿಕ",
-  description: "ಚನ್ನಗಿರಿ ಅಡಿಕೆ ಕಾರ್ಮಿಕ ವೇದಿಕೆ"
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="kn">
-      <body
-        style={{
-          margin: 0,
-          padding: 0,
-          fontFamily: "Arial, sans-serif"
-        }}
-      >
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
