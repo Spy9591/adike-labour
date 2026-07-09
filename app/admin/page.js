fcc2c4487afd9272afb3f1407f51e38157a09af2
Spy9591 <93334073+Spy9591@users.noreply.github.com>
2026-07-09 13:44:47 +0530

    Create page.js

---
+++ app/admin/page.js
@@ -0,0 +1,15 @@
+export default function AdminPage() {
+  return (
+    <div
+      style={{
+        padding: "40px",
+      }}
+    >
+      <h1>Admin Dashboard</h1>
+
+      <p>
+        Firebase connection successful ✅
+      </p>
+    </div>
+  );
+}
