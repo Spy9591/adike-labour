190880260a00027e4489ba170b3c6c841fe6136f
Spy9591 <93334073+Spy9591@users.noreply.github.com>
2026-07-09 13:53:27 +0530

    Update page.js

--- app/admin/page.js
+++ app/admin/page.js
@@ -2,14 +2,15 @@ export default function AdminPage() {
   return (
     <div
       style={{
+        minHeight: "100vh",
+        background: "#0f172a",
+        color: "white",
         padding: "40px",
       }}
     >
-      <h1>Admin Dashboard</h1>
+      <h1>📊 Admin Dashboard</h1>
 
-      <p>
-        Firebase connection successful ✅
-      </p>
+      <p>Firebase Connected Successfully ✅</p>
     </div>
   );
 }
