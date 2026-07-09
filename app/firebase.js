31a50b05a59cc0d3f384081ab3b98a9f0e7974d4
Spy9591 <93334073+Spy9591@users.noreply.github.com>
2026-07-09 13:43:51 +0530

    Update firebase.js

--- app/firebase.js
+++ app/firebase.js
@@ -8,12 +8,8 @@ const firebaseConfig = {
   storageBucket: "channagiri-labour-platform.firebasestorage.app",
   messagingSenderId: "534300835463",
   appId: "1:534300835463:web:8f90650c97941111da8222",
-  measurementId: "G-FCEZZXJ691",
 };
 
 const app = initializeApp(firebaseConfig);
 
-export default app;
-export { app };
-
-export { getFirestore };
+export const db = getFirestore(app);
